06165af59f7a375a7d466b45894831473a56bbb9
ovad4221 <andrey22ovs@yandex.ru>
2024-04-22 04:03:58 +0300

    symmetric Gauss-Seidel and fastest gradient descent

--- src/headers_of_meth/simple_iter_methods.hpp
+++ src/headers_of_meth/simple_iter_methods.hpp
@@ -18,4 +18,10 @@ namespace iter_methods {
 [[nodiscard]] Vector chebishev_accel(const CSR& csr, const Vector& x_0, const Vector& b, double l_min, double l_max,
         unsigned n_cheb_roots, double required_accuracy, size_t iteration_limit_by_n);
 
+[[nodiscard]] Vector symmetric_gauss_zeidel(const CSR& csr, Vector& x_0, const Vector& b, 
+        double required_accuracy, unsigned frequency_checking, unsigned iteration_limit);
+
+[[nodiscard]] Vector fastest_gradient_descent(const CSR& csr, const Vector& x_0, const Vector& b,
+        double required_accuracy, unsigned frequency_checking, unsigned iteration_limit);
+
 }
--- src/methods/simple_iter_methods.cpp
+++ src/methods/simple_iter_methods.cpp
@@ -89,3 +89,62 @@
     return res;
 }
 
+// Это сработает если реально вторая часть так просто делается, Коля, проверь меня
+[[nodiscard]] Vector iter_methods::symmetric_gauss_zeidel(const CSR& csr, Vector& x_0, const Vector& b, 
+        double required_accuracy, unsigned frequency_checking, unsigned iteration_limit) {
+
+    double d = -1;
+    for (unsigned k = 0u; k < iteration_limit; k += frequency_checking) {
+        if ((csr * x_0 - b).module_v() < required_accuracy) return x_0;
+        // оберточный цикл
+        for (size_t h = 0u; h < frequency_checking; ++h) {
+
+            // сам метод Гаусса-Зейделя
+            for (size_t i = 0; i < x_0.get_size(); ++i) {
+                x_0[i] = b.get_value(i);
+                for (size_t j = csr.get_rows()[i]; j < csr.get_rows()[i + 1]; ++j) {
+                    if (csr.get_cols()[j] != i) {
+                        x_0[i] -= csr.get_values()[i] * x_0[csr.get_rows()[i]];
+                    } else {
+                        d = csr.get_values()[j];
+                    }
+                }
+                x_0[i] /= d;
+            }
+
+            // опа, симметризация Гаусса-Зейделя (вторая часть)
+            for (size_t i = x_0.get_size() - 1; i-- > 0;) {
+                x_0[i] = b.get_value(i);
+                for (size_t j = csr.get_rows()[i]; j < csr.get_rows()[i + 1]; ++j) {
+                    if (csr.get_cols()[j] != i) {
+                        x_0[i] -= csr.get_values()[i] * x_0[csr.get_rows()[i]];
+                    } else {
+                        d = csr.get_values()[j];
+                    }
+                }
+                x_0[i] /= d;
+            }
+        }
+    }
+
+    return x_0;
+}
+
+[[nodiscard]] Vector iter_methods::fastest_gradient_descent(const CSR& csr, const Vector& x_0, const Vector& b,
+        double required_accuracy, unsigned frequency_checking, unsigned iteration_limit) {
+    Vector res = x_0;
+    Vector r = csr * res - b;
+    for (size_t i = 0u; i < iteration_limit; i += frequency_checking) {
+        if (r.module_v() < required_accuracy) return res;
+
+        for (unsigned j = 0u; j < frequency_checking; ++j) {
+            // решение одномерной минимизации
+            r = csr * res - b;
+            res = res - (r * r / (r * (csr * r))) * (csr * res - b);
+        }
+    }
+    // return -1;
+    return res;
+}
+
+
